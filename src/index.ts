33b47acc5b65a29e394bacf5c1f73a060a56f768
Rohit Yadav <rohityadav.techie@gmail.com>
2023-03-27 17:23:05 +0530

    refector code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-export * as useLocalStorageState from './hooks/use-local-storage';
+import * as React from 'react';
+
+/**
+ *
+ * @param {String} key The key to set in localStorage for this value
+ * @param {Object} defaultValue The value to use if it is not already in localStorage
+ * @param {{serialize: Function, deserialize: Function}} options The serialize and deserialize functions to use (defaults to JSON.stringify and JSON.parse respectively)
+ */
+
+type Serializer<T> = {
+  serialize: (value: T) => string;
+  deserialize: (str: string) => T;
+};
+
+const useLocalStorageState = <T>(
+  key: string,
+  defaultValue: T | (() => T) = {} as T,
+  { serialize = JSON.stringify, deserialize = JSON.parse }: Serializer<T> = {
+    serialize: JSON.stringify,
+    deserialize: JSON.parse,
+  },
+): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const [state, setState] = React.useState<T>(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key);
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage);
+    }
+    return typeof defaultValue === 'function'
+      ? (defaultValue as () => T)()
+      : defaultValue;
+  });
+
+  const prevKeyRef = React.useRef(key);
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
+
+  return [state, setState];
+};
+
+export default useLocalStorageState;
